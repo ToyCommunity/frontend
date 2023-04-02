d0a71980eb0343c6583905f01ec51d72ebe2e253
ssabii <fhy1930@gmail.com>
2023-04-02 23:58:16 +0900

    chore: global style 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,6 @@ import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 
-import '@/styles/globals.css'
-
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
