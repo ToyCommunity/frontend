779a136351517578046edeadd04520873c98b8ac
GoDoyeon <97godo@naver.com>
2023-04-05 00:04:44 +0900

    Feat : 로그인 페이지 버튼 추가 ( css 작업 전)

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Link from 'next/link'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 
 function Login() {
   return (
     <div>
-      로그인 페이지
+      <h1>로그인 페이지</h1>
+      <TextField id="outlined-basic" label="id" variant="outlined" />
+      <TextField id="outlined-basic" label="password" variant="outlined" />
+      <Link href="/">
+        <Button variant="contained">로그인</Button>
+      </Link>
+      <Link href="signup">
+        <Button variant="contained">회원가입</Button>
+      </Link>
     </div>
   )
 }
