7cf6b7ac808e7b3d72d8367cb0d688908baa07fd
ssabii <fhy1930@gmail.com>
2023-04-18 20:57:04 +0900

    feat: auth mock api 작성

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,20 +1,59 @@
-import api from './api';
-
-interface Todo {
-  id: number;
-  userId: number;
-  title: string;
-  completed: boolean;
+export interface SignUpRequest {
+  email: string;
+  password: string;
+  nickname: string;
 }
 
-export const todoApi = {
-  get: async () => {
-    const { data } = await api.get<Todo[]>('/todos');
+export interface SignUpResponse {
+  email: string;
+  nickname: string;
+}
 
-    return data;
-  }
+// eslint-disable-next-line no-unused-vars
+const signUp = async ({ email, password, nickname }: SignUpRequest) => {
+  const res = await new Promise<SignUpResponse>((resolve) => {
+    const signUpResponse: SignUpResponse = {
+      email,
+      nickname,
+    };
+    
+    setTimeout(() => {
+      resolve(signUpResponse);
+    }, 300);
+  });
+
+  return res;
 };
 
-/**위 예제는 샘플 코드입니다.
- * auth api가 정의되면 이 파일을 수정해주세요.
- * */ 
+export interface SignInRequest {
+  email: string;
+  password: string;
+}
+
+export interface SignInResponse {
+  accessToken: string;
+  accessTokenExpiredTime: number;
+  refreshToken: string;
+  refreshTokenExpiredTime: number;
+}
+
+// eslint-disable-next-line no-unused-vars
+const signIn = async ({ email, password }: SignInRequest) => {
+  const signInResponse: SignInResponse = {
+    accessToken: 'eyJhbGciOiJIUzUxMiJ9.eyJpZCI6MSwiaWF0IjoxNjgxNjUxMDc1LCJleHAiOjE2ODE2NTQ2NzV9.AnbPfMwqA_RnLtNonR21Ip0gHLGjqop2OZ3tIBoJYnJW_Bn55GkbeVBm8DLVocKNvSsy5MNzlWC2j0pMoU0q4g',
+    accessTokenExpiredTime: 1000 * 60 * 60,
+    refreshToken: 'eyJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE2ODE2NTEwNzUsImV4cCI6MTY4MTczNzQ3NX0.delW-Pi-lrzmEjJcIT0HVFpJ_C1_7CvompZACl_3XCzWsJ-4NdLuzMFDXxTSGE0JSQuoOaNS4_BmrJdH4Cmp7Q',
+    refreshTokenExpiredTime: 1000 * 60 * 60 * 24,
+  };
+
+  const res = await new Promise<SignInResponse>((resolve) => {
+    setTimeout(() => resolve(signInResponse), 300);
+  });
+
+  return res;
+};
+
+export const authApi = {
+  signUp,
+  signIn,
+};
