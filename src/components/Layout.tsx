6906aed2ba2c7e0ad723bb42fc6851d2728db4e5
GoDoyeon <97godo@naver.com>
2023-04-18 23:21:01 +0900

    Feat : layout 로그인, 회원가입 페이지 분기처리

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,25 +2,38 @@
 import Header from './Header'
 import Footer from './Footer'
 import { Box } from '@mui/material'
+import { useRouter } from 'next/router'
 
 interface Props {
     children: any
 }
 
 const Layout = ({children}: Props) => {
-    return (
-        <>
-            <Header/>
-            <Box
-                sx={{ 
-                    borderTop: "1px solid #ebebeb",
-                    borderBottom: "1px solid #ebebeb"
-                }}
-            >
-                {children}
-            </Box>
-            <Footer/>
-        </>
-    )
+    const router = useRouter();
+    if (router.route === '/signin' || router.route === '/signup') {
+        return (
+            <>
+                <Box>
+                    {children}
+                </Box>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <Header/>
+                <Box
+                    sx={{ 
+                        borderTop: "1px solid #ebebeb",
+                        borderBottom: "1px solid #ebebeb"
+                    }}
+                >
+                    {children}
+                </Box>
+                <Footer/>
+            </>
+        )
+
+    }
 }
 export default Layout;
