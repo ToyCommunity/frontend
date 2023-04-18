6bb670831ca7dc7cc069ad99a86a97499d2d4bd8
ssabii <fhy1930@gmail.com>
2023-04-18 23:16:17 +0900

    feat: global style 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 
+import '@/styles/globals.css';
+
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
