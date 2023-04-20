e9a1a6200a8d0e83a55db15efdc2e55bb2473f78
ssabii <fhy1930@gmail.com>
2023-04-21 00:38:41 +0900

    feat: modularizeImports 설정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
-/** @type {import('next').NextConfig} */
 const removeImports = require('next-remove-imports')();
 
+/** @type {import('next').NextConfig} */
 const nextConfig = removeImports({
   reactStrictMode: true,
+  swcMinify: true,
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    }
+  }
 });
 
 
