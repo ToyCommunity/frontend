289fa072fcdb9ae3f5694a8e8895c933c24366ba
ssabii <fhy1930@gmail.com>
2023-04-20 22:56:15 +0900

    feat: props 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,38 +1,39 @@
 
-import Header from './Header'
-import Footer from './Footer'
-import { Box } from '@mui/material'
-import { useRouter } from 'next/router'
+import Header from './Header';
+import Footer from './Footer';
+import { Box } from '@mui/material';
+import { useRouter } from 'next/router';
 
 interface Props {
-    children: any
+  children: React.ReactNode;
 }
 
-const Layout = ({children}: Props) => {
-    const { pathname } = useRouter();
-    const pathnames = ['/signin', '/signup'];
-    if (pathnames.includes(pathname)) {
-        return (
-            <Box>
-                {children}
-            </Box>
-        )
-    } else {
-        return (
-            <>
-                <Header/>
-                <Box
-                    sx={{ 
-                        borderTop: "1px solid #ebebeb",
-                        borderBottom: "1px solid #ebebeb"
-                    }}
-                >
-                    {children}
-                </Box>
-                <Footer/>
-            </>
-        )
+const Layout = ({ children }: Props) => {
+  const { pathname } = useRouter();
+  const pathnames = ['/signin', '/signup'];
 
-    }
-}
+  if (pathnames.includes(pathname)) {
+    return (
+      <Box>
+        {children}
+      </Box>
+    );
+  } else {
+    return (
+      <>
+        <Header />
+        <Box
+          sx={{
+            borderTop: "1px solid #ebebeb",
+            borderBottom: "1px solid #ebebeb"
+          }}
+        >
+          {children}
+        </Box>
+        <Footer />
+      </>
+    );
+
+  }
+};
 export default Layout;
