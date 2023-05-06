b552bb29a95cc93cc443cc90a80bd38ad8e78402
ssabii <fhy1930@gmail.com>
2023-05-07 01:43:48 +0900

    feat: 공통 에러 응답 구현

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,17 @@
 import axios from 'axios';
+import { AxiosError } from 'axios';
+
+export interface ErrorDetail {
+  code: string;
+  message: string;
+}
+
+export interface ErrorResponse {
+  result: string,
+  detail: ErrorDetail,
+}
+
+AxiosError<>
 
 const api = typeof window === 'undefined'
   ? axios.create({
