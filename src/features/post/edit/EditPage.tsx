f78ec3a08d3255403adf85ede0e03d02daf54541
ssabii <fhy1930@gmail.com>
2023-06-04 01:25:36 +0900

    feat: 글 수정 구현

--- src/features/post/edit/EditPage.tsx
+++ src/features/post/edit/EditPage.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
 import { QueryClient, dehydrate, useQuery } from '@tanstack/react-query';
 import { ONLY_NUMBER_REGEX } from '@/utils';
 import { postApi } from '@/api';
 import { useRouter } from 'next/router';
+import { Button, Container, Stack, TextField } from '@mui/material';
+import MDEditor from '@/components/MDEditor';
+import { useBeforeUnload } from '@/hooks/common';
 
 function EditPage() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   const router = useRouter();
   const id = Number(router.query?.id);
 
-  const { data } = useQuery(
+  const { data: detail } = useQuery(
     ['postDetail', id],
     ({ signal }) => postApi.getDetailPost({ postId: id, signal }),
     {
@@ -21,8 +26,71 @@ function EditPage() {
     }
   );
 
+  useEffect(() => {
+    setTitle(detail?.title || '');
+    setContent(detail?.content || '');
+  }, [detail]);
+
+  useBeforeUnload({});
+
+  const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleChangeContent = (value?: string) => {
+    setContent(value || '');
+  };
+
+  const updatePost = async () => {
+    await postApi.updatePost({
+      postId: id,
+      title,
+      content
+    });
+    router.push(`/post/${id}`);
+  };
+
   return (
-    <div>EditPage</div>
+    <Container
+      maxWidth="md"
+      sx={{ marginTop: '16px' }}
+    >
+      <TextField
+        placeholder='제목을 입력해주세요.'
+        margin="normal"
+        value={title}
+        onChange={handleChangeTitle}
+        fullWidth
+      />
+      <MDEditor
+        height={500}
+        minHeight={500}
+        toolbarHeight={56}
+        value={content}
+        textareaProps={{ placeholder: '내용을 입력해주세요.' }}
+        onChange={handleChangeContent}
+      />
+      <Stack
+        marginY={1}
+        gap={1}
+        direction="row"
+        display='flex'
+        justifyContent='flex-end'
+      >
+        <Button
+          href="/"
+          variant="outlined"
+        >
+          취소
+        </Button>
+        <Button
+          variant="contained"
+          onClick={updatePost}
+        >
+          수정
+        </Button>
+      </Stack>
+    </Container >
   );
 }
 
